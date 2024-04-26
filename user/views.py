b45e6933457bfd86f06252d35befc9a602e10836
= <=>
2024-04-26 22:16:50 +0400

    login, register and other user functions have been prepared

--- user/views.py
+++ user/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import (
+    LoginSerializer, RegisterSerializer, ResetPasswordSerializer, ResetPasswordCheckSerializer,
+    ResetPasswordCompleteSerializer, ChangePasswordSerializer
+)
+from django.contrib.auth import get_user_model
+from django.utils.encoding import smart_str
+from django.utils.http import urlsafe_base64_decode
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+User = get_user_model()
+
+
+class LoginView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = LoginSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class ResetPasswordView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = ResetPasswordSerializer
+
+
+class ResetPasswordCheckView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = ResetPasswordCheckSerializer
+    lookup_field = "uuid"
+
+    def get_object(self):
+        # convert uuid to id and get user
+        uuid = self.kwargs.get(self.lookup_field)
+        id_ = smart_str(urlsafe_base64_decode(uuid))
+        return User.objects.get(id=int(id_))
+
+
+class ResetPasswordCompleteView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = ResetPasswordCompleteSerializer
+    lookup_field = "uuid"
+
+    def get_object(self):
+        # convert uuid to id and get user
+        uuid = self.kwargs.get(self.lookup_field)
+        id_ = smart_str(urlsafe_base64_decode(uuid))
+        return User.objects.get(id=int(id_))
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = ChangePasswordSerializer
+    permission_classes = IsAuthenticated,
+
+    def get_object(self):
+        return self.request.user
+
+
+
+
+
