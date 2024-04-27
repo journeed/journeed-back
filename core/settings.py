0a61a076fc5481fd54242ab1440b407915310aaf
= <=>
2024-04-27 10:46:53 +0400

    login with token is enabled in settings

--- core/settings.py
+++ core/settings.py
@@ -127,8 +127,8 @@ AUTH_USER_MODEL = "user.User"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        # 'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication'
     )
 }
 
