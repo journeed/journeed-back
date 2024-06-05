cedfb2410c866c10653935fd5884daa2e2ea49f8
Aslan Gahramanov <aslanqehramanovofficial@gmail.com>
2024-06-05 12:39:01 +0400

    test-prod

--- core/asgi.py
+++ core/asgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+from dotenv import load_dotenv
+
 
 from django.core.asgi import get_asgi_application
-
+load_dotenv()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
